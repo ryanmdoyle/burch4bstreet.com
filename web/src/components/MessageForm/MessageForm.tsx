266388aa28fa341409cd54e5b90efa44317dc390
Ryan <ryandoyle@me.com>
2021-05-29 14:45:53 -0700

    add recaptcha

--- web/src/components/MessageForm/MessageForm.tsx
+++ web/src/components/MessageForm/MessageForm.tsx
@@ -6,10 +6,19 @@ import {
   TextField,
   Submit,
 } from '@redwoodjs/forms'
+import ReCAPTCHA from 'react-google-recaptcha'
+import { useState } from 'react'
 
 const MessageForm = (props) => {
+  const [recap, setRecap] = useState(null)
   const onSubmit = (data) => {
-    props.onSave(data, props?.message?.id)
+    if (recap !== null) {
+      props.onSave(data, props?.message?.id)
+    }
+  }
+
+  const onChange = (value) => {
+    setRecap(value)
   }
 
   return (
@@ -50,7 +59,12 @@ const MessageForm = (props) => {
           defaultValue={props.message?.email}
           className="rw-input"
           errorClassName="rw-input rw-input-error"
-          validation={{ required: true }}
+          validation={{
+            required: true,
+            pattern:
+              // eslint-disable-next-line no-control-regex
+              /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+          }}
         />
         <FieldError name="email" className="rw-field-error text-red-500" />
 
@@ -86,10 +100,19 @@ const MessageForm = (props) => {
         />
         <FieldError name="message" className="rw-field-error text-red-500" />
 
+        <div className="flex justify-center mt-8">
+          <ReCAPTCHA
+            sitekey={'6LeqBf0aAAAAAPR1sMETC7i0lNvYtMkGkW6NNOxp'}
+            onChange={onChange}
+          />
+        </div>
+
         <div className="rw-button-group">
           <Submit
-            disabled={props.loading}
-            className="rw-button bg-sofiaGreen hover:bg-sofiaGreenDark text-white"
+            disabled={recap === null}
+            className={`${
+              recap === null ? 'cursor-not-allowed' : ''
+            } rw-button bg-sofiaGreen hover:bg-sofiaGreenDark text-white disabled:opacity-50`}
           >
             Send!
           </Submit>
