373b1bc6fb9f9e4c5ce02d99754222019e20e5cc
Ryan Doyle <doylecodes@gmail.com>
2021-05-31 16:56:02 -0700

    footer for disclaimer

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -2,14 +2,14 @@ import { Toaster } from '@redwoodjs/web/toast'
 
 const MainLayout: React.FunctionComponent = ({ children }) => {
   return (
-    <div className="w-screen min-h-screen background">
+    <div className="w-screen min-h-screen background flex flex-col justify-between">
       <Toaster
         position="bottom-right"
         toastOptions={{ success: { duration: 5000 } }}
       />
-      <div className="container mx-auto py-8">
+      <section className="container mx-auto py-8">
         {/* Image and Page Title */}
-        <section className="pt-4 px-4 md:p-8 lg:h-1/3 w-full flex flex-col lg:flex-row justify-center items-center">
+        <div className="pt-4 px-4 md:p-8 lg:h-1/3 w-full flex flex-col lg:flex-row justify-center items-center">
           <div className="w-1/2 lg:w-1/4 xl:w-1/5 lg:h-full flex justify-center items-center">
             <img
               src="/lyndsay.jpeg"
@@ -29,16 +29,34 @@ const MainLayout: React.FunctionComponent = ({ children }) => {
               Artistic Director
             </span>
           </div>
-        </section>
+        </div>
         {/* Dividing Line */}
         <div className="flex flex-col justify-center items-center my-6">
           <div className="w-3/4 h-0.5 bg-sofiaGreen"></div>
         </div>
         {/* Page Content */}
-        <section className="w-full flex flex-col justify-center px-8 pb-8 lg:pt-4 overflow-hidden">
+        <div className="w-full flex flex-col justify-center px-8 pb-8 lg:pt-4 overflow-hidden">
           {children}
-        </section>
-      </div>
+        </div>
+      </section>
+      <section className="bg-sofiaGreen">
+        <div className="container mx-auto py-8">
+          <h2 className="text-white text-2xl font-display text-center mb-4">
+            Who We Are
+          </h2>
+          <div className="flex flex-col justify-center items-center my-6">
+            <div className="w-1/3 h-0.5 bg-white"></div>
+          </div>
+          <p className="text-white text-center p-8">
+            Who are we? We are a group of actors, audience members, friends, and
+            community members who wanted to create an easy way for others who
+            support Lyndsay Burch to be heard by the B Street Board of
+            Directors. We created this site out of our love for Lyndsay and B
+            Street, and while Lyndsay is aware of this site, she has had no
+            involvement in its creation or maintenance.{' '}
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
