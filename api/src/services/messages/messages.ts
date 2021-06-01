84c8cf7eb0b9780c7e1e548e7abe8de2732b1696
Ryan Doyle <doylecodes@gmail.com>
2021-06-01 12:37:11 -0700

    remove nodemailer/transmail services

--- api/src/services/messages/messages.ts
+++ api/src/services/messages/messages.ts
@@ -4,7 +4,6 @@ import { requireAuth } from 'src/lib/auth'
 import { BeforeResolverSpecType } from '@redwoodjs/api'
 import { ServerClient } from 'postmark'
 
-import nodemailer from 'nodemailer'
 import md5 from 'md5'
 import sanitizeHtml from 'sanitize-html'
 
@@ -68,42 +67,9 @@ export const createMessage = async ({ input }: CreateMessageArgs) => {
       message: sanitizedMessage,
     },
   })
-  // const transporter = nodemailer.createTransport({
-  //   host: 'smtp.transmail.com',
-  //   port: 587,
-  //   auth: {
-  //     user: process.env.ZOHO_USER,
-  //     pass: process.env.ZOHO_PASS,
-  //   },
-  // })
-
-  // CONFIRMATION
-  // await transporter.sendMail({
-  //   from: '"Support Lyndsay" <supportlyndsay@burch4bstreet.com>', // sender address
-  //   to: input.email, // list of receivers
-  //   subject: `Confirmation - ${input.subject}`, // Subject line
-  //   text: input.message, // plain text body
-  //   html: `
-  //   <h1 style="color:#2DBFB0;">Thanks for the support!</h1>
-  //   <p>We really appreciate the time you took to support Lyndsay. Take a look at a copy of the message sent below.</p>
-  //   <p>${input.message}</p>
-  //   `, // html body
-  // })
-
-  // TO THE BOARD
-  // await transporter.sendMail({
-  //   from: '"Support Lyndsay" <supportlyndsay@burch4bstreet.com>', // sender address
-  //   to: process.env.BOARD_EMAILS, // list of receivers
-  //   subject: `${input.subject} - From ${input.from}`, // Subject line
-  //   text: input.message, // plain text body
-  //   html: `
-  //   <h1 style="color:#2DBFB0;">In support of Lyndsay Burch!</h1>
-  //   <p>${input.message}</p>
-  //   `, // html body
-  // })
-  const emailHash = md5(input.email)
 
   // use md5 to hash email before saving (can be used to check for duplicates)
+  const emailHash = md5(input.email)
   return db.message.create({
     data: {
       from: input.from,
